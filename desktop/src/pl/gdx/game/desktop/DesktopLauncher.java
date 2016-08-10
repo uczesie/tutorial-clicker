12aa51f972a8ba9b82de1fb6bb8bf78df384a9b5
Admin-Komputer\Admin <wojtek100503@gmail.com>
2016-08-10 19:27:46 +0200

    no message

---
+++ desktop/src/pl/gdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.gdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.gdx.game.TutorialClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TutorialClickerGame(), config);
+	}
+}
