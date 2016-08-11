7eefe935eec6b1a522d6f8b63cc9a95059bd930b
Admin-Komputer\Admin <wojtek100503@gmail.com>
2016-08-11 20:47:32 +0200

    Display Player image

--- core/src/pl/gdx/game/screens/GameplayScreen.java
+++ core/src/pl/gdx/game/screens/GameplayScreen.java
@@ -1,8 +1,11 @@
 package pl.gdx.game.screens;
 
 import pl.gdx.game.TutorialClickerGame;
+import pl.gdx.game.entities.Player;
 
 public class GameplayScreen extends AbstractScreen{
+
+	private Player player;
 
 	public GameplayScreen(TutorialClickerGame game) {
 		super(game);
@@ -10,8 +13,28 @@ public class GameplayScreen extends AbstractScreen{
 	}
 
 	private void init() {
-		// TODO Auto-generated method stub
+		initPlayer();
 		
+	}
+
+	private void initPlayer() {
+		player = new Player();
+		stage.addActor(player);
+		
+	}
+	
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		update();
+		
+		spriteBatch.begin();
+		stage.draw();
+		spriteBatch.end();
+	}
+
+	private void update() {
+		stage.act();	
 	}
 
 }
