43f56b485118b39068734129e97bc935e77c24ca
Admin-Komputer\Admin <wojtek100503@gmail.com>
2016-08-11 12:15:56 +0200

    config desktop launcher

--- core/src/pl/gdx/game/TutorialClickerGame.java
+++ core/src/pl/gdx/game/TutorialClickerGame.java
@@ -1,33 +1,40 @@
 package pl.gdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class TutorialClickerGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import pl.gdx.game.screens.SplashScreen;
+
+public class TutorialClickerGame extends Game {
+	
+	public final static String GAME_NAME = "Tutorial Clicker";
+	
+	public final static int WIDTH = 480;
+	public final static int HEIGHT = 700;
+	
+	private boolean paused;
+	
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		this.setScreen(new SplashScreen(this));
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+	/**
+     * ---------------------
+     * getters and setters
+     * 
+     */
+	
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public void setPaused(boolean paused) {
+		this.paused = paused;
 	}
 }
--- desktop/src/pl/gdx/game/desktop/DesktopLauncher.java
+++ desktop/src/pl/gdx/game/desktop/DesktopLauncher.java
@@ -5,8 +5,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import pl.gdx.game.TutorialClickerGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new TutorialClickerGame(), config);
+
+		config.title = TutorialClickerGame.GAME_NAME;
+		config.height = TutorialClickerGame.HEIGHT;
+		config.width = TutorialClickerGame.WIDTH;
+		config.resizable = false;
+		
 	}
 }
