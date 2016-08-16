d0edfb19c1b7f7afb50d7f8a4b37e75297e8d001
Admin-Komputer\Admin <wojtek100503@gmail.com>
2016-08-16 10:32:27 +0200

    Saving points

--- core/src/pl/gdx/game/TutorialClickerGame.java
+++ core/src/pl/gdx/game/TutorialClickerGame.java
@@ -2,6 +2,7 @@ package pl.gdx.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,6 +11,8 @@ import pl.gdx.game.screens.SplashScreen;
 
 public class TutorialClickerGame extends Game {
 	
+	public final static String GAME_PREFS = "pl.gdx.game.prefs";
+	public final static String GAME_SCORE = "pl.gdx.game.prefs.score";
 	public final static String GAME_NAME = "Tutorial Clicker";
 	
 	public final static int WIDTH = 480;
@@ -17,16 +20,31 @@ public class TutorialClickerGame extends Game {
 	
 	private boolean paused;
 	
+	private Preferences prefs;
+	
 	private int points;
 	
 	
 	@Override
 	public void create () {
+		init();
 		this.setScreen(new SplashScreen(this));
 	}
 	
+	private void init() {
+		prefs = Gdx.app.getPreferences(GAME_PREFS);
+		loadScore();
+	}
+
+	private void loadScore() {
+		points= prefs.getInteger(GAME_SCORE);
+		
+	}
+
 	public void addPoint(){
 		points++;
+		prefs.putInteger(GAME_SCORE, points);
+		prefs.flush();
 	}
 
 	/**
